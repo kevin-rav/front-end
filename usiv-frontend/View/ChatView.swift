20ec57bd681a56ea9df59d7e33ec46d7b731837c
Hower <haowen.chen@qq.com>
2024-02-19 19:04:18 -0500

    added chatting

---
+++ usiv-frontend/View/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  usiv-frontend
+//
+//  Created by Haowen Chen on 2/19/24.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
